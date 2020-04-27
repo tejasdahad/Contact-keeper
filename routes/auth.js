eb8e5b92dc5eb967a8c5c3f81dd8a55e22c9163f
Tejas Dahad <tejasdahad000@gmail.com>
2020-04-27 13:45:33 +0530

    Auth middleware and protecting routes, completed auth

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@ const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const {check,validationResult} = require('express-validator/check');
+const auth = require('../middleware/auth');
 
 // @route   GET api/auth
 // @desc    Get user
@@ -55,8 +56,14 @@ router.post('/', [
 // @route   POST api/auth
 // @desc    Log in a user
 // @access  Public
-router.get('/', (req,res) => {
-    res.send('Login a user');
+router.get('/', auth, async (req,res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
